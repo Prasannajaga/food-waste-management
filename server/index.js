c0656f383106518db85718eb82b44d367ebc0727
notharshagit <harshashetty256@gmail.com>
2025-02-06 15:53:34 +0530

    Setup Sequelize with PostgreSQL

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-import express  from "express";
+import express from "express";
+import sequelize from "./models/index.js"; // Import sequelize setup (adjust path if needed)
 
+const app = express();
 
-const app  = express();
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-
-app.get("/" , (req, res) =>{
-    res.send("hello")
+app.get("/", (req, res) => {
+  res.send("Hello, the API is running");
 });
 
-
-app.listen(3000);
-
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
