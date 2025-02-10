1abbbb6472d0000c7def55eae15d006f8b95e339
prasannajaga <prasannajaga9@gmail.com>
2025-02-10 23:39:57 +0530

    supabase db connection implemented

--- server/src/config/dbClient.ts
+++ server/src/config/dbClient.ts
@@ -1,19 +1,28 @@
-import { Dialect, Sequelize } from "sequelize";
-import { DB_CONFIG } from "./dbconfig";
+import { Sequelize } from "sequelize";
+import { DB_URI, } from "./dbconfig";
 
 // Database configuration
-export const sequelize = new Sequelize(DB_CONFIG.database, DB_CONFIG.username, DB_CONFIG.password, {
-  host: DB_CONFIG.host,
-  dialect: DB_CONFIG.dialect as Dialect,
-  port: DB_CONFIG.port, // Default MySQL port
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+// export const sequelize = new Sequelize(DB_CONFIG.database, DB_CONFIG.username, DB_CONFIG.password, {
+//   host: DB_CONFIG.host,
+//   dialect: DB_CONFIG.dialect as Dialect,
+//   port: DB_CONFIG.port, // Default MySQL port
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+// });
 
+export const sequelize = new Sequelize(DB_URI, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for Supabase
+    },
+  },
+});
 
 
 // Test connection function
