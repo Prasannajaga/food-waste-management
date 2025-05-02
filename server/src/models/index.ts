c82845989b8bc7bd678d905e48f47dd36cf0a4ba
prasanna <prasannajaga9@gmail.com>
2025-05-02 23:22:53 +0530

    claim api done

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,6 +3,7 @@ import User from "./Users";
 import Post from "./Post";
 import Comment from "./comment";
 import Likes from "./Like";
+import Claim from "./claim";
  
 Post.hasMany(Likes, { as: 'Likes', foreignKey: 'post_id' });
 Post.hasMany(Comment, { as: 'Comments', foreignKey: 'post_id' });
@@ -15,8 +16,18 @@ Likes.belongsTo(Post, { foreignKey: 'post_id' });
 Comment.belongsTo(Post, { foreignKey: 'post_id' });
 Comment.belongsTo(User, { foreignKey: 'user_id' });
 
+Claim.belongsTo(User, {
+foreignKey: 'claimer_id',
+as: 'claimer',
+});
+
+User.hasMany(Claim, {
+foreignKey: 'claimer_id',
+as: 'claims',
+});
+  
 // models/User.js
 User.hasMany(Post, { foreignKey: 'user_id' });
 User.hasMany(Comment, { foreignKey: 'user_id' });
-
-export { User, Post , Comment , Likes};
+ 
+export { User, Post , Comment , Likes , Claim};
